7a7980ab81133e817178897f6e01fc7292aa49e3
lanshee-world <keyafidel@gmail.com.>
2026-02-18 14:15:35 +0300

    soko

---
+++ app.py
@@ -0,0 +1,51 @@
+# import flask and its components
+from flask import *
+
+# import the pymysql module-It helps us to create a connection between python flask and mysql database
+import pymysql
+
+# Create a flask application and give it a name
+app = Flask(__name__)
+
+# Below is the sign up route
+@app.route("/api/signup" , methods = ["POST"])
+def signup():
+    if request.method == "POST":
+        # Extract the different details entered on the form
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        phone = request.form["phone"]
+
+        # By use of the print function let's print all those details sent with the upcoming request
+        # print(username, email, password, phone)
+
+        # establish a connection between flask/python and mysql
+        connection = pymysql.connect(host ="localhost", user ="root", password ="", database ="sokogardenonline")
+
+        # Create a cursor to execute the sql queries
+        cursor = connection.cursor()
+
+        # structure a sql to insert the details received from the form
+        # the %s is a placeholder-> a place holder stands in places of actual values i.e we shall replace later on
+        sql = "INSERT INTO users(username,email,phone,password) VALUES(%s,%s,%s,%s)"
+
+        # create a tuple that will hold all the data gotten from the form
+        data = (username, email, phone, password)
+
+        # by use of the cursor execute the sql as you replace the place holders with the actual values
+        cursor.execute(sql, data)
+
+        # commit the changes to the database
+        connection.commit()
+
+        return jsonify({"message" : "User registered successfully"})
+
+
+
+
+
+
+
+# run the application
+app.run(debug=True)
